74999a526a19cd11f0da8e8f10efd9f76c87f733
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-18 20:50:55 +0200

    Update

--- scripts.js
+++ scripts.js
@@ -1,22 +1,21 @@
 const salary = 4000;
-const lodging = 'apartment'
-const size = 'large'
-
+const lodging = 'apartment';
+const size = 'large';
 
 // Only change the syntax below (not the values or key names)
 
 const expenses = {
     food: 51.7501,
-    transport:  10.2,
-}
-  
+    transport: 10.2,
+};
+
 const tax = {
     734: '3%',
     234: '20%',
     913: '12%',
     415: '38%',
     502: '42%',
-}
+};
 
 const rent = {
     none: 0,
@@ -26,13 +25,13 @@ const rent = {
     'large-apartment': 800,
     'small-house': 1200,
     'large-house': 2400,
-}
+};
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
- 
+const taxAsDecimal = parseFloat(tax[913]) / 100;
+const startingAfterTax = salary * (1 - taxAsDecimal);
+const type = lodging + '-' + size; // Corrected this line
+const balance = startingAfterTax - expenses.transport - expenses.food - rent['large-apartment'];
+
+console.log(balance.toFixed(2));
